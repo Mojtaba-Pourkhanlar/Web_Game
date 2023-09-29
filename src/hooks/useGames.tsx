0da55b12a797ead54e6744ba74d764fd3dbff6fd
“[Mojtaba-Pourkhanlar]” <[mojtabapourkhanlar@gmail.com]>
2023-09-29 14:16:16 +0330

    fixeed loading-icon-image

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -3,10 +3,18 @@ import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
-    id: number,
-    name: string,
-    background_image: string
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
 }
 
 interface FetchGamesResponse {
@@ -17,24 +25,30 @@ const useGames = () => {
 
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const controller = new AbortController();
-
+        setLoading(true)
         apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal })
-            .then(res => setGames(res.data.results))
+            .then(res => {
+                setLoading(false)
+                setGames(res.data.results)
+            })
             .catch((err) => {
                 if (err instanceof CanceledError) {
                     return;
                 }
                 setError(err.message)
+                setLoading(false)
+
             })
 
         return () => controller.abort();
 
     }, [])
 
-    return { games, error }
+    return { games, error, loading }
 }
 
 export default useGames
