9b36ff46e8d64b3c5fb1a677c5fadf8b5d23c1db
“[Mojtaba-Pourkhanlar]” <[mojtabapourkhanlar@gmail.com]>
2023-10-03 12:39:31 +0330

    clean up the game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,8 +19,8 @@ const GameGrid = ({ gameQuery }: Props) => {
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{
-                sm: 1, md: 2, lg: 3, xl: 5
-            }} spacing={3} padding="10px">
+                sm: 1, md: 2, lg: 3, xl: 4
+            }} spacing={6} padding="10px">
 
                 {loading && skeleton.map((skelet) => <GameCardContainer key={skelet}> <GameCardSkeleton /></GameCardContainer>)}
 
